f18902bbaa93f1493b742ded0254488b2831b2fc
Gogorillo <rouyer.pierre.pro@gmail.com>
2023-01-10 19:45:14 +0100

    Add the validation into the train loop

--- dataset.py
+++ dataset.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset
 import numpy as np
 from PIL import Image
 import torch
-
+import torch.nn.functional as F
 
 train = pd.read_csv(r"C:\Users\piclt\Desktop\Ecole\4A\ProCom\Data\train.csv")
 
@@ -21,13 +21,22 @@ def getMask3D(MRPath):
     for im in os.listdir(MRPath):
         l.append(np.asarray(Image.open(os.path.join(MRPath,im))).astype("int16"))
     return np.asarray(l)
+
+def ResizeGridSample(X_init, size, is_mask=False):
+    shape = X_init.shape
+    grid = torch.tensor([shape[0], size, size, 2]).float()
+    print(grid)
+    if is_mask:
+        return F.grid_sample(X_init, grid, mode='nearest')
     
+    return F.grid_sample(X_init, grid)
          
 class Dataset(Dataset):
     'characterizes a dataset for pytorch'
-    def __init__(self, df, transform=None, mask_transform=None):
+    def __init__(self, df, size=100, transform=False):
         self.df = df
         self.transform = transform
+        self.size = size
 
     def __len__(self):
         'denotes the total number of samples'
@@ -41,16 +50,23 @@ class Dataset(Dataset):
         X = getDicom3D(path_mr)
         y = getMask3D(path_mask)
         
-        X = X[np.newaxis,np.newaxis,:,:,:]  #np.shape(X) = (256,256,1)
+        X = X[np.newaxis,np.newaxis,:,:,:]  
         X = from_numpy(X).float()
         y = y[np.newaxis,np.newaxis,:,:,:]
         y = from_numpy(y).float()
+
+        
+        if self.transform:
+            X = self.transform(X)
+            y = self.transform(y)    
+        
         return X, y
+    
 if __name__ == "__main__":
     df= pd.read_csv(r"C:\Users\piclt\Desktop\Ecole\4A\ProCom\Data\train.csv")
     path_mr = os.path.join(df["root"].iloc[0], df["localImPath"].iloc[0])
     path_mask = os.path.join(df["root"].iloc[0], df["localMaskPath"].iloc[0])
     ar1 = getDicom3D(path_mr)
     ar2 = getMask3D(path_mask)
-    print(ar2.dtype)
+    ar1 = from_numpy(ar1).float()
     
--- train.py
+++ train.py
@@ -6,40 +6,77 @@ from torch.optim import Adam
 import models 
 from loss import DiceBCELoss
 import pandas as pd
+import os
+
+def saveModel(epoch, loss): 
+    path = os.path.join(savePath, f"epoch{epoch}_loss{loss}_model.pth")
+    torch.save(model.state_dict(), path) 
 
 
-model = models.UNet3D(in_channels=1, out_channels=3)
-criterion = DiceBCELoss()
-optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+def train(model, criterion, optimizer, trainloader, testloader, N_epoch, savePath):
+    
+    best_val_loss = 0.0
+    
+    for epoch in range(N_epoch):  # loop over the dataset multiple times
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        print(f"epoch {epoch} is starting")
+        
+        running_loss = 0.0
+        running_val_loss = 0.0 
+        
+        for i, data in enumerate(trainloader, 1):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            print(inputs.shape)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+    
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = running_loss(outputs, labels)
+            loss.backward()
+            optimizer.step()
+    
+            # print statistics
+            running_loss += loss.item()
+            
+            with torch.no_grad(): 
+                model.eval() 
+                for data in testloader: 
+                   inputs, outputs = data 
+                   predicted_outputs = model(inputs) 
+                   val_loss = criterion(predicted_outputs, outputs) 
+                 
+                   running_val_loss += val_loss.item()  
+ 
+        # Save the model if the accuracy is the best 
+        if running_val_loss > best_val_loss: 
+            saveModel(epoch, running_val_loss) 
+            best_val_loss = running_val_loss 
+         
+        # Print the statistics of the epoch 
+        print('Completed training batch', epoch, 'Training Loss is: %.4f' %running_loss, 'Validation Loss is: %.4f' %running_val_loss)
 
-batch_size = 1
-N_epoch = 20
-
-trainset = pd.read_csv(r"C:\Users\piclt\Desktop\Ecole\4A\ProCom\Data\train.csv")
-
-trainloader = Dataset(trainset)
-
-for epoch in range(N_epoch):  # loop over the dataset multiple times
-
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 1):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = model(inputs)
-        loss = running_loss(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
 
 
-print('Finished Training')
+if __name__ == "__main__":
+    
+    savePath = r"C:\Users\piclt\Desktop\Ecole\4A\ProCom\Data\Save_model"
+    model = models.UNet3D(in_channels=1, out_channels=3)
+    criterion = DiceBCELoss()
+    optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    batch_size = 1
+    N_epoch = 20
+
+    trainset = pd.read_csv(r"C:\Users\piclt\Desktop\Ecole\4A\ProCom\Data\train.csv")
+    testset = pd.read_csv(r"C:\Users\piclt\Desktop\Ecole\4A\ProCom\Data\test.csv")
+    trainloader = Dataset(trainset)
+    testloader = Dataset(testset)
+    
+    train(model, criterion, optimizer, trainloader, testloader, N_epoch, savePath)
+    print('Finished Training')
